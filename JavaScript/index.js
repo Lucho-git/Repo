b892bb63f13542281708029643fa57b5027cff48
lachlan.f.ross@gmail.com <lachlan.f.ross@gmail.com>
2020-04-21 22:47:34 +0800

    making some test files to try and replicate a complicated screen overlay to expand from mobile view or zoomed in screens

--- JavaScript/index.js
+++
@@ -1,7 +0,0 @@
-//Javascript code starts here
-console.log('hello world');
-let person = {name:'Lach'};
-console.log(person);
-let interestRate = 1;
-console.log(interestRate);
-console.log(typeof(person));
