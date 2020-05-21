e2d9ee041b3d57c1d4a45cbc123a24038d094cbb
lachlan.f.ross@gmail.com <lachlan.f.ross@gmail.com>
2020-05-21 19:41:47 +0800

    learned p5, use for code visualization on website, created a snake game on projects section of webpage

---
+++ Css Grid/p5js/snake.js
@@ -0,0 +1,21 @@
+
+class snake {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.xspeed = 1;
+        this.yspeed = 0;
+        this.dir = function (x, y) {
+            this.xspeed = x;
+            this.yspeed = y;
+        };
+        this.update = function () {
+            this.x = this.x + this.xspeed;
+            this.y = this.y + this.yspeed;
+        };
+        this.show = function () {
+            fill(255);
+            rect(this.x, this.y, 10, 10);
+        };
+    }
+}
